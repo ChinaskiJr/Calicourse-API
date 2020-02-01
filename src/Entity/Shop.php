1d72374d72acc6432652c4756d3deed0f17aa2a0
ChinaskiJr <robin.colombier@protonmail.com>
2020-02-01 14:27:26 +0100

    Remove http patch methods for both article or shop

--- src/Entity/Shop.php
+++ src/Entity/Shop.php
@@ -8,8 +8,11 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ApiResource()
- * @ORM\Entity(repositoryClass="App\Repository\ShopRepository")
+ * @ApiResource(
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={"get", "put", "delete"}
+ * )
+ *  @ORM\Entity(repositoryClass="App\Repository\ShopRepository")
  */
 class Shop
 {
