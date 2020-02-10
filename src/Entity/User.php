2c1a55ca6ee69ed759d2ab898d0163dabc14addb
ChinaskiJr <robin.colombier@protonmail.com>
2020-02-10 21:14:39 +0100

    Add apiToken attribute to the User class

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -53,6 +53,11 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true, unique=true)
+     */
+    private $apiToken;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -124,5 +129,17 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getApiToken(): ?string
+    {
+        return $this->apiToken;
+    }
+
+    public function setApiToken(?string $apiToken): self
+    {
+        $this->apiToken = $apiToken;
+
+        return $this;
     }
 }
