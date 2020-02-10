04c7cce016f862f04e8482c395d34289e01ef917
ChinaskiJr <robin.colombier@protonmail.com>
2020-02-10 23:05:12 +0100

    Enable Api Token authentification

---
+++ src/Security/TokenAuthenticator.php
@@ -0,0 +1,108 @@
+<?php
+
+
+namespace App\Security;
+
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class TokenAuthenticator extends AbstractGuardAuthenticator
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        $data = [
+            'message' => 'Authentification demandée'
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supports(Request $request)
+    {
+        return $request->headers->has('X-AUTH-TOKEN');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCredentials(Request $request)
+    {
+        return [
+            'token' => $request->headers->get('X-AUTH-TOKEN'),
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $apiToken = $credentials['token'];
+
+        if (null === $apiToken) {
+            return null;
+        }
+
+        // if a User object, checkCredentials() is called
+        return $this->em->getRepository(User::class)
+            ->findOneBy(['apiToken' => $apiToken]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $data = [
+            'message' => "Clé API invalide"
+        ];
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
